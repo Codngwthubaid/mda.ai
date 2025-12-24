cdaf64fb08b6517892f8973ce60161fe70436ab3
Codngwthubaid <codngwthubaid@gmail.com>
2025-12-24 12:02:14 +0530

    feat: implement authentication in header and add Kinde auth middleware

--- app/(routes)/_common/header.tsx
+++ app/(routes)/_common/header.tsx
@@ -1,13 +1,23 @@
 "use client";
+
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { LogOutIcon, MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 import Link from "next/link";
+import { LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Header = () => {
     const { theme, setTheme } = useTheme();
     const isDark = theme === "dark";
+
+    const { user, getUser } = useKindeBrowserClient();
+    const alsoUser = getUser();
+    console.log(user);
+
     return (
         <div className="sticky top-0 right-0 left-0 z-30">
             <header className="h-16 border-b bg-background py-4">
@@ -57,8 +67,36 @@ const Header = () => {
                                 )}
                             />
                         </Button>
-                        <Button>Sign in</Button>
-
+                        {user ? (
+                            <DropdownMenu>
+                                <DropdownMenuTrigger>
+                                    <Avatar className="h-8 w-8 shrink-0 rounded-full">
+                                        <AvatarImage
+                                            src={user?.picture || ""}
+                                            alt={user?.given_name || ""}
+                                        />
+                                        <AvatarFallback className="rounded-lg">
+                                            {user?.given_name?.charAt(0)}
+                                            {user?.family_name?.charAt(0)}
+                                        </AvatarFallback>
+                                    </Avatar>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent className="w-56" align="end">
+                                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                                    <DropdownMenuSeparator />
+                                    <DropdownMenuItem>
+                                        <LogoutLink className="w-full flex items-center">
+                                            <LogOutIcon className="size-4" />
+                                            Logout
+                                        </LogoutLink>
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        ) : (
+                            <LoginLink>
+                                <Button>Sign in</Button>
+                            </LoginLink>
+                        )}
                     </div>
                 </div>
             </header>
